93e4d9cd4c5b6101ea24dc0232b134497b0b8da2
albarey04 <146063672+albarey04@users.noreply.github.com>
2024-05-14 18:15:06 +0200

    subscriber_member_function.py

--- practica3/practica3/subscriber_member_function.py
+++ practica3/practica3/subscriber_member_function.py
@@ -1,3 +1,5 @@
+import math
+
 import rclpy
 from rclpy.node import Node
 
@@ -5,33 +7,55 @@ from std_msgs.msg import String
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
 
+from practica3.publisher_member_function import MinimalPublisher
+
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.velocity_subscription = self.create_subscription(
-            Twist,
-            '/turtle1/cmd_vel',
-            self.velocity_callback,
-            10)
-        self.velocity_subscription  # prevent unused variable warning
+        #self.velocity_subscription1 = self.create_subscription(Twist, '/turtle1/cmd_vel', self.velocity_callback, 10)
+        #self.velocity_subscription1 # prevent unused variable warning
 
-        self.pose_subscription = self.create_subscription(
-            Pose,
-            '/turtle1/pose',
-            self.pose_callback,
-            10)
-        self.pose_subscription  # prevent unused variable warning
+        #self.pose_subscription1 = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
+        #self.pose_subscription1  # prevent unused variable warning
 
-    def velocity_callback(self, msg):
-        #leer velocidades
-        self.get_logger().info(f'Received velocity - linear.x: {msg.linear.x}, angular.z: {msg.angular.z}')
+        #self.velocity_subscription = self.create_subscription(Twist,'/turtle2/cmd_vel',self.velocity_callback,10)
+        #self.velocity_subscription  # prevent unused variable warning
 
-    def pose_callback(self, msg):
+        #self.pose_subscription = self.create_subscription(Pose,'/turtle2/pose',self.pose_callback,10)
+        #self.pose_subscription  # prevent unused variable warning
+
+        #self.velocity_subscription3 = self.create_subscription(Twist, '/turtle3/cmd_vel', self.velocity_callback, 10)
+        #self.velocity_subscription3 # prevent unused variable warning
+
+        #self.pose_subscription3 = self.create_subscription(Pose, '/turtle3/pose', self.pose_callback, 10)
+        #self.pose_subscription3  # prevent unused variable warning
+
+        self.minimal_publisher = MinimalPublisher()
+
+
+
+    def pose_callback1(self,msg):
+        self.t1_x = msg.x
+        self.t1_y = msg.y
+        self.t1_theta = msg.theta
         # leer en que posicion estamos
         self.get_logger().info(f'Received pose -x: {msg.x}, y: {msg.y}, theta: ´{msg.theta}')
 
+    def pose_callback2(self,msg):
+        self.t2_x = msg.x
+        self.t2_y = msg.y
+        self.t2_theta = msg.theta
+        # leer en que posicion estamos
+        self.get_logger().info(f'Received pose -x: {msg.x}, y: {msg.y}, theta: ´{msg.theta}')
+
+    def pose_callback3(self,msg):
+        self.t3_x = msg.x
+        self.t3_y = msg.y
+        self.t3_theta = msg.theta
+        # leer en que posicion estamos
+        self.get_logger().info(f'Received pose -x: {msg.x}, y: {msg.y}, theta: ´{msg.theta}')
 
 def main(args=None):
     rclpy.init(args=args)

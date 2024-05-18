8410cace25e88ec47f35a2802feaf3306ddcc095
albarey04 <146063672+albarey04@users.noreply.github.com>
2024-05-18 13:16:44 +0200

    subscriber_member_function.py

--- practica3/practica3/subscriber_member_function.py
+++ practica3/practica3/subscriber_member_function.py
@@ -9,32 +9,12 @@ from geometry_msgs.msg import Twist
 
 from practica3.publisher_member_function import MinimalPublisher
 
-
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        #self.velocity_subscription1 = self.create_subscription(Twist, '/turtle1/cmd_vel', self.velocity_callback, 10)
-        #self.velocity_subscription1 # prevent unused variable warning
-
-        #self.pose_subscription1 = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
-        #self.pose_subscription1  # prevent unused variable warning
-
-        #self.velocity_subscription = self.create_subscription(Twist,'/turtle2/cmd_vel',self.velocity_callback,10)
-        #self.velocity_subscription  # prevent unused variable warning
-
-        #self.pose_subscription = self.create_subscription(Pose,'/turtle2/pose',self.pose_callback,10)
-        #self.pose_subscription  # prevent unused variable warning
-
-        #self.velocity_subscription3 = self.create_subscription(Twist, '/turtle3/cmd_vel', self.velocity_callback, 10)
-        #self.velocity_subscription3 # prevent unused variable warning
-
-        #self.pose_subscription3 = self.create_subscription(Pose, '/turtle3/pose', self.pose_callback, 10)
-        #self.pose_subscription3  # prevent unused variable warning
-
+        
         self.minimal_publisher = MinimalPublisher()
-
-
 
     def pose_callback1(self,msg):
         self.t1_x = msg.x
@@ -65,12 +45,9 @@ def main(args=None):
     rclpy.spin(minimal_subscriber)
     #spin, ejecuta ros por detras, cuando le llega un call trabaja
     #spin_once: fa una sola iteracio
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
+    
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
